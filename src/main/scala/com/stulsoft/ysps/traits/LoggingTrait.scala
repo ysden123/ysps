b02deeb1adc32ca37d5ebe924bb70ae0c7a63cb1
ysden123 <ysden123@gmail.com>
2016-02-26 10:20:59 +0200

    Using 'with'

--- src/main/scala/com/stulsoft/ysps/traits/LoggingTrait.scala
+++ src/main/scala/com/stulsoft/ysps/traits/LoggingTrait.scala
@@ -4,6 +4,8 @@
 
 package com.stulsoft.ysps.traits
 
+import java.lang.StackTraceElement
+
 /**
  * Playing with traits - trait for logging.
  *
@@ -14,7 +16,11 @@ object LoggingTrait {
 
 	def main(args: Array[String]): Unit = {
 		println("==>main")
-		val t1 = new TestClass(15)
+		val t = new TestClass(15)
+		t.foo(25)
+		
+		val t1 = new TestClass1(16)
+		t1.foo()
 		println("<==main")
 	}
 }
@@ -27,6 +33,12 @@ object LoggingTrait {
  */
 trait Log {
 	def log(message: String) = println(message)
+	def log2(message: String): Unit = {
+		val stackTraceElements: Array[StackTraceElement] = Thread.currentThread().getStackTrace()
+		println(s"Class: ${stackTraceElements(3).getClassName}")
+		println(s"Method: ${stackTraceElements(3).getMethodName}")
+		println(s"Message: $message")
+	}
 }
 
 /**
@@ -37,4 +49,32 @@ trait Log {
  */
 class TestClass(val a1: Int) extends Log {
 	log(s"a1=$a1")
+
+	def foo(i: Int): Unit = {
+		log2(s"i=$i")
+	}
+}
+
+/**
+ * Just a trait
+ * 
+ * @author Yuriy Stul
+ *
+ */
+trait T123 {
+	def foo(): Unit
+}
+
+/**
+ * Test class for T123 with Log
+ * 
+ * @author Yuriy Stul
+ *
+ */
+class TestClass1(val a1: Int) extends T123 with Log {
+	log2("a1 = " + a1)
+	def foo(): Unit = {
+		println("The foo")
+		log2("The foo")
+	}
 }
