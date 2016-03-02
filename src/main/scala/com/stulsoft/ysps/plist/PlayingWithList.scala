5f5b707995e8dc437601c25ac7cf37520902b7a2
ysden123 <ysden123@gmail.com>
2016-03-02 23:31:49 +0200

    Collection operators

--- src/main/scala/com/stulsoft/ysps/plist/PlayingWithList.scala
+++ src/main/scala/com/stulsoft/ysps/plist/PlayingWithList.scala
@@ -8,12 +8,13 @@ import scala.collection.mutable.ListBuffer
 
 /**
  * Playing with List: ListBuffer.
- * 
+ *
  * @author Yuriy Stul
  *
  */
 object PlayingWithList {
 	def main(args: Array[String]) {
+		println("==>main")
 		val b1 = new ListBuffer[Int]()
 		b1.+=(1)
 		println(b1)
@@ -39,10 +40,25 @@ object PlayingWithList {
 		for (i <- Range(0, 1)) {
 			println(s"i=$i, b1($i)=${b1(i)}")
 		}
-		
+
 		println(s"b1.size=${b1.size}")
-		
+
 		b1(0) = 100
 		println(b1)
+
+		val digits = List(4, 2)
+		println(s"digits=$digits")
+		println(s"digits.head=${digits.head}")
+		println(s"digits.tail=${digits.tail}")
+		println(s"digits.tail.tail=${digits.tail.tail}")
+		
+		val digits2 = 9 :: digits
+		println(s"digits2=$digits2")
+		
+		println(s"digits ++ digits2=${digits ++ digits2}")
+		println(s"digits +: (digits2)=${digits +: (digits2)}")
+		println(s"digits :+ (digits2)=${digits :+ (digits2)}")
+		println(s"digits ::: (digits2)=${digits ::: (digits2)}")
+		println("<==main")
 	}
 }
