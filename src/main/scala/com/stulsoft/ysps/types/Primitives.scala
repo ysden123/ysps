4d70f02024dd4f7cc530cd794e13e330597eb3f3
ysden123 <ysden123@gmail.com>
2016-10-07 13:02:55 +0300

    future

---
+++ src/main/scala/com/stulsoft/ysps/types/Primitives.scala
@@ -0,0 +1,15 @@
+package com.stulsoft.ysps.types
+
+/**
+  * Created by Yuriy Stul on 10/4/2016.
+  */
+object Primitives {
+  def main(args: Array[String]): Unit = {
+    var i:Int = 1
+    i += 3
+    println(s"i = $i")
+
+    i = 987
+    println(s"i = $i")
+  }
+}
