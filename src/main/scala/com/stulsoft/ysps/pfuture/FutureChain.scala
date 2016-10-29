f559a03fb04f394d02774185b21a339aba37850d
ysden123 <ysden123@gmail.com>
2016-10-29 18:05:56 +0300

    FutureChain

---
+++ src/main/scala/com/stulsoft/ysps/pfuture/FutureChain.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016. StulSoft, Yuriy Stul
+ */
+
+package com.stulsoft.ysps.pfuture
+
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+/**
+  * Demonstrates Future chain using flatMap
+  * Created by Yuriy Stul on 10/29/2016.
+  */
+object FutureChain extends App with LazyLogging {
+  logger.info("Start")
+
+  // 1st future
+  val f1 = Future {
+    Thread.sleep(500)
+    "F1"
+  }
+
+  // 2nd future. Waits completion of the 1st future
+  val f2 = f1.flatMap {
+    theF1 =>
+      Future {
+        Thread.sleep(500)
+        s"$theF1 + 2"
+      }
+  }
+
+  // 2nd future. Waits completion of the 1st future
+  val f3 = f2.flatMap {
+    theF2 =>
+      Future {
+        Thread.sleep(500)
+        theF2 + "+ 3"
+      }
+  }
+
+  f3 onSuccess {
+    case completedResult => logger.info("Completed result {}", completedResult)
+  }
+
+  Thread.sleep(1600)
+
+  logger.info("End")
+}
