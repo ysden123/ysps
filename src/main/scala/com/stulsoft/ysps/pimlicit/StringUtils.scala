2c7fb7906f277a819fcedc821daa8ed6a2c7458d
ysden123 <ysden123@gmail.com>
2016-11-08 21:37:58 +0200

    Implicit class

---
+++ src/main/scala/com/stulsoft/ysps/pimlicit/StringUtils.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016. StulSoft, Yuriy Stul
+ */
+
+package com.stulsoft.ysps.pimlicit
+
+/**
+  * Created by Yuriy Stul on 11/8/2016.
+  */
+object StringUtils {
+
+  /**
+    *
+    * @param s a string
+    */
+  implicit class ImplicitClass(s: String) {
+
+    /**
+      * Increments each character
+      *
+      * @return incremented string
+      */
+    def increment = s.map(c => (c + 1).toChar)
+  }
+
+}
+
