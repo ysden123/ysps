96ecf62ca40dc57e1beaafba42d88774e42587b7
ysden123 <ysden123@gmail.com>
2016-11-17 20:18:57 +0200

    Playing with Vector

--- src/main/scala/com/stulsoft/ysps/pcollection/PVector.scala
+++ src/main/scala/com/stulsoft/ysps/pcollection/PVector.scala
@@ -1,8 +1,23 @@
+/*
+ * Copyright (c) 2016. Yuriy Stul
+ */
+
 package com.stulsoft.ysps.pcollection
 
 /**
+  * Playing with Vector.
+  *
   * Created by Yuriy Stul on 11/17/2016.
   */
-object PVector {
+object PVector extends App {
+  println("==>Start")
 
+  val v1 = Vector("aaa", "bbb", "ccc")
+
+  // Concatenate
+  val r1 = v1.reduceLeft((v1, v2) => v1 + "\t" + v2)
+  println(s"r1: $r1")
+
+  println(s"""r2: ${v1.mkString("\t")}""")
+  println("==>End")
 }
