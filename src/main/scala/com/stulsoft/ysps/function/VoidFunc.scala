744aac95402ffb7ad7b2daa64a0b7d08da949157
ysden123 <ysden123@gmail.com>
2016-11-25 19:11:40 +0200

    T

--- src/main/scala/com/stulsoft/ysps/function/VoidFunc.scala
+++ src/main/scala/com/stulsoft/ysps/function/VoidFunc.scala
@@ -14,6 +14,9 @@ object VoidFunc extends App {
 
   /**
     * A function with function as argument
+    *
+    * Allows usage a function without arguments and returns nothing (Unit)
+    *
     * @param f a function without arguments; doesn't return a result (Unit)
     */
   def foo(f: () => Unit): Unit = {
@@ -22,6 +25,21 @@ object VoidFunc extends App {
     println("<==foo")
   }
 
-  foo(() => println("In the f"))
+  /**
+    * A function with function as argument
+    *
+    * Allows usage a function without arguments and returns any type and Unit too.
+    *
+    * @param f a function without arguments; returns any type (T) and nothing (Unit) too
+    * @tparam T specifies return type for f function
+    */
+  def foo2[T](f: () => T): Unit = {
+    println("==>foo2")
+    f()
+    println("<==foo2")
+  }
+
+  foo(() => println("In the f for foo"))
+  foo2(() => println("In the f for foo2"))
   println("<==main")
 }
