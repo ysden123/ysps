5429963eb4720ce9ad434c373a62dbf621cf66b0
ysden123 <ysden123@gmail.com>
2016-12-27 21:10:15 +0200

    Usage compose function of the List class

--- src/main/scala/com/stulsoft/ysps/pcompose/PCompose.scala
+++ src/main/scala/com/stulsoft/ysps/pcompose/PCompose.scala
@@ -27,6 +27,29 @@ object PCompose extends App {
     println("<==test1")
   }
 
+  /**
+    * Usage compose function of the List class
+    */
+  private def test2(): Unit = {
+    println("==>test2")
+    val f1: Int => Int = i => i * 2
+    val l1 = List(10, 20, 30, 40, 50, 60)
+
+    /**
+      * fc1 is the function: l1(f1(i)) - returns an element
+      * with index that is equal value of f1(i). It is equal to l1.apply(f1(i))
+      */
+    val fc1 = l1.compose(f1)
+    println(s"l1: ${l1.mkString(", ")}")
+    println(s"fc1(1)=${fc1(1)}; l1(2)=${l1(2)}")
+    println(s"fc1(2)=${fc1(2)}; l1(4)=${l1(4)}")
+    println(s"l1.apply(4)=${l1.apply(4)}")
+
+    println("<==test2")
+  }
+
   test1()
+  println()
+  test2()
   println("<==PCompose")
 }
