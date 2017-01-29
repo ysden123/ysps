d3d88807edd0e00efeaa799bc50874b498dbe5e0
ysden123 <ysden123@gmail.com>
2017-01-29 20:39:09 +0200

    playing with XYXTag

---
+++ src/main/scala/com/stulsoft/ysps/generics/ClassTagEx01.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2017. Yuriy Stul
+ */
+
+package com.stulsoft.ysps.generics
+
+import scala.reflect.ClassTag
+
+/**
+  * Playing ClassTag
+  *
+  * @see [[https://medium.com/byte-code/overcoming-type-erasure-in-scala-8f2422070d20#.pq8ocqkbt Overcoming type erasure in Scala]]
+  * @author Yuriy Stul
+  */
+object ClassTagEx01 extends App {
+
+  val list = List(1, "string1", List(), "string2")
+  val result1 = Extractor1.extract[String](list)
+
+  println("==>main")
+  val result2 = Extractor2.extract[String](list)
+
+  println("Extractor1")
+
+  /**
+    * Wrong, warning in compilation.
+    */
+  object Extractor1 {
+    def extract[T](list: List[Any]): List[T] = list.flatMap {
+      case element: T => Some(element)
+      case _ => None
+    }
+  }
+
+  println(result1) // List(1, string1, List(), string2)
+
+  println("Extractor2")
+
+  /**
+    * Correct
+    */
+  object Extractor2 {
+    def extract[T](list: List[Any])(implicit tag: ClassTag[T]): List[T] =
+      list.flatMap {
+        case element: T => Some(element)
+        case _ => None
+      }
+  }
+
+  println(result2) // List(string1, string2)
+  println("<==main")
+}
