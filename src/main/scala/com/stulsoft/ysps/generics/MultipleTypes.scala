17fee8e1c0c1918cd06c742a76e7a816ac9f371a
Yuriy Stul <yuriy.stul@williamhill.com>
2017-01-30 11:40:54 +0200

    type

---
+++ src/main/scala/com/stulsoft/ysps/generics/MultipleTypes.scala
@@ -0,0 +1,26 @@
+package com.stulsoft.ysps.generics
+
+/**
+  * Playing with multiple types in generics.
+  *
+  * <p>
+  * Usage the <i>type</i>
+  *
+  * @author Yuriy Stul.
+  */
+object MultipleTypes extends App {
+  type NumberType = (Double, Int, Long, Float) => Number
+
+  def f(x: Number): Unit = {
+    println(s"x = $x")
+  }
+
+  println("==>main")
+  f(1)
+  f(2.0)
+  f(3.0E-1)
+  f(4L)
+  // Error: type mismatch
+//  f("text")
+  println("<==main")
+}
