260f31167a0fe3a408a262b84bc84f3c78b26ce5
ysden123 <ysden123@gmail.com>
2017-02-01 20:29:31 +0200

    classTag

--- src/main/scala/com/stulsoft/ysps/generics/ClassTagEx02.scala
+++ src/main/scala/com/stulsoft/ysps/generics/ClassTagEx02.scala
@@ -4,7 +4,8 @@ import scala.reflect.{ClassTag, classTag}
 
 /**
   * Playing with ClassTag.
-  *@see [[https://scalerablog.wordpress.com/2015/12/21/classtag-class-and-war-stories/ ClassTag, Class and war stories…]]
+  *
+  * @see [[https://scalerablog.wordpress.com/2015/12/21/classtag-class-and-war-stories/ ClassTag, Class and war stories…]]
   * @author Yuriy Stul.
   */
 object ClassTagEx02 extends App {
@@ -12,9 +13,23 @@ object ClassTagEx02 extends App {
     println(s"classTag[T].getClass: ${classTag[T].getClass}")
     println(s"classTag[T].runtimeClass: ${classTag[T].runtimeClass}")
     println(s"classTag[T].runtimeClass.isAssignableFrom(classTag[String].runtimeClass): ${classTag[T].runtimeClass.isAssignableFrom(classTag[String].runtimeClass)}")
+
+    val a1: Array[T] = Array.ofDim(5)
+    println(s"a1: $a1")
+
+    val theType = x match {
+      case _: java.lang.String => "String"
+      case _: java.lang.Double => "Double"
+      case _: java.lang.Float => "Float"
+      case _ => "Unsupported"
+    }
+    println(s"theType is $theType")
   }
 
   println("==>main")
   f1("text")
+  f1(1.0)
+  f1[Float](1.0F)
+  f1(1)
   println("<==main")
 }
