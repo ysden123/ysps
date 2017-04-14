60587449ef4277100172cf49f3909063da75192f
ysden123 <ysden123@gmail.com>
2017-04-14 11:36:13 +0300

    example 1

---
+++ src/main/scala/com/stulsoft/ysps/forcomprehansiion/Example1.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017. Yuriy Stul
+ */
+
+package com.stulsoft.ysps.forcomprehansiion
+
+/**
+  * @author Yuriy Stul
+  */
+object Excample1 extends App {
+  val books = generateBooks()
+  val r1 = for {
+    book <- books
+    if book.author endsWith "2"
+  } yield book.title
+  val r2 = books.withFilter(book => book.author endsWith "2").map(book => book.title)
+  println(s"books: $books")
+  println(s"r1: $r1")
+  println(s"r2: $r2")
+
+  def generateBooks(): Seq[Book1] = {
+    (1 to 3).map(i => Book1(s"author$i", s"title$i"))
+  }
+}
+
+case class Book1(author: String, title: String)
+
