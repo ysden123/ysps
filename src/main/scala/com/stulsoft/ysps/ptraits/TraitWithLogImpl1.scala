1382eea50f10ff42c056fc1a329adb5697f0169a
Yuriy Stul <yuriy.s@dauup-clicksmob.com>
2018-04-02 08:25:13 +0300

    trait with log 2

--- src/main/scala/com/stulsoft/ysps/ptraits/TraitWithLogImpl1.scala
+++ src/main/scala/com/stulsoft/ysps/ptraits/TraitWithLogImpl1.scala
@@ -4,6 +4,6 @@ package com.stulsoft.ysps.ptraits
   * @author Yuriy Stul
   * @since 4/1/2018
   */
-class TraitWithLogImpl1 {
+class TraitWithLogImpl1 extends TraitWithLog {
 
 }
--- src/main/scala/com/stulsoft/ysps/ptraits/TraitWithLogRunner.scala
+++ src/main/scala/com/stulsoft/ysps/ptraits/TraitWithLogRunner.scala
@@ -4,6 +4,9 @@ package com.stulsoft.ysps.ptraits
   * @author Yuriy Stul
   * @since 4/1/2018
   */
-object TraitWithLogRunner {
-
+object TraitWithLogRunner extends App {
+  val t1 = new TraitWithLogImpl1
+  val t2 = new TraitWithLogImpl2
+  t1.makeLog()
+  t2.makeLog()
 }
