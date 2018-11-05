388b3722509a8d204c48719adec26388602503ea
Yuriy Stul <ysden123@gmail.com>
2018-11-05 19:21:46 +0200

    Using URL

--- src/main/scala/com/stulsoft/ysps/pio/ReadProperties.scala
+++ src/main/scala/com/stulsoft/ysps/pio/ReadProperties.scala
@@ -4,32 +4,18 @@
 
 package com.stulsoft.ysps.pio
 
-import java.io.File
 import java.net.URL
-import java.nio.file.{Path, Paths}
+import java.util.Properties
 
 /**
   * @author Yuriy Stul
   */
 object ReadProperties extends App {
-  val dir = "file:///C:/configuration/panel/development/app_customer.properties"
-  val fn=new File(dir).toURI.toURL
-  println(s"fn: $fn")
+  //  c:\work\test\app.properties
+  val dir = "file:///C:/work/test/app.properties"
+  var url = new URL(dir)
 
-  val fn2=new File(dir).toURI
-  println(s"fn2: $fn2")
-
-  val fn3=new File(dir).getAbsolutePath
-  println(s"fn3: $fn3")
-
-  val fn4=new File(dir)
-  println(s"fn4: $fn4")
-
-  val f=fn4.getAbsoluteFile
-  println(s"f: $f")
-
-  val url = new URL(dir)
-  val strem=url.openStream()
-
-
+  val props = new Properties()
+  props.load(url.openStream())
+  props.forEach { (name, value) => println(s"name=$name, value=$value") }
 }
