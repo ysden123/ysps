42ed041f603375c00d1892e1ff366637e12f7b52
Yuriy Stul <ysden123@gmail.com>
2019-01-17 22:26:40 +0200

    Option with sum

---
+++ src/main/scala/com/stulsoft/ysps/poption/POptionSum.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2019. Yuriy Stul
+ */
+
+package com.stulsoft.ysps.poption
+
+/**
+  * @author Yuriy Stul
+  */
+object POptionSum extends App {
+  def func1(items: Seq[Container]): Option[Double] = {
+    val forSum = items
+      .filter(_.total.isDefined)
+      .map(_.total.get)
+
+    if (forSum.nonEmpty)
+      Option(forSum.sum)
+    else
+      Option.empty
+  }
+
+  def func2(items: Seq[Container]): Option[Double] = {
+    val forSum = for (
+      item <- items;
+      t <- item.total
+    ) yield t
+
+    if (forSum.nonEmpty)
+      Option(forSum.sum)
+    else
+      Option.empty
+  }
+
+  def test1(): Unit = {
+    println("==>test1")
+    var result = func1(List(Container("1", Option(1.0)), Container("2", Option.empty)))
+    if (result.isDefined)
+      println(s"result=${result.get}")
+    else
+      println("Undefined")
+
+    result = func1(List(Container("1", Option(1.0)), Container("2", Option(2.0))))
+    if (result.isDefined)
+      println(s"result=${result.get}")
+    else
+      println("Undefined")
+
+    result = func1(List(Container("1", Option.empty), Container("2", Option.empty)))
+    if (result.isDefined)
+      println(s"result=${result.get}")
+    else
+      println("Undefined")
+    println("<==test1")
+  }
+
+  def test2(): Unit = {
+    println("==>test2")
+    var result = func2(List(Container("1", Option(1.0)), Container("2", Option.empty)))
+    if (result.isDefined)
+      println(s"result=${result.get}")
+    else
+      println("Undefined")
+
+    result = func2(List(Container("1", Option(1.0)), Container("2", Option(2.0))))
+    if (result.isDefined)
+      println(s"result=${result.get}")
+    else
+      println("Undefined")
+
+    result = func2(List(Container("1", Option.empty), Container("2", Option.empty)))
+    if (result.isDefined)
+      println(s"result=${result.get}")
+    else
+      println("Undefined")
+    println("<==test2")
+  }
+
+  println("Start")
+  test1()
+  test2()
+  println("End")
+}
+
+case class Container(name: String, var total: Option[Double])
