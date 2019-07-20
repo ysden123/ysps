6ba1e83a3dc6d4a2d979821dff93fa60b6d9881a
Yuriy Stul <ysden123@gmail.com>
2019-07-20 15:45:56 +0300

    Scala 13
    Scala 13

--- src/main/scala/com/stulsoft/ysps/pstream/InfinityStreamRandom.scala
+++ src/main/scala/com/stulsoft/ysps/pstream/InfinityStreamRandom.scala
@@ -11,14 +11,14 @@ object InfinityStreamRandom extends App {
 
   def test1(): Unit = {
     println("==>test1")
-    Stream.continually(Random.nextInt(100)).take(10).foreach(println)
+    LazyList.continually(Random.nextInt(100)).take(10).foreach(println)
 
     println("<==test1")
   }
 
   def test2(): Unit = {
     println("==>test2")
-    val i = Stream.continually(Random.nextInt(100)).iterator
+    val i = LazyList.continually(Random.nextInt(100)).iterator
     (1 to 10).foreach(_ => println(i.next()))
     println("<==test2")
   }
--- src/main/scala/com/stulsoft/ysps/pstream/StreamEx1.scala
+++ src/main/scala/com/stulsoft/ysps/pstream/StreamEx1.scala
@@ -10,7 +10,7 @@ object StreamEx1 extends App {
 
   def test1(): Unit = {
     println("==>test1")
-    lazy val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
+    lazy val fibs: LazyList[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
     println(s"fibs.take(10)=${fibs take 10 foreach println}")
     println("<==test1")
   }
@@ -19,7 +19,7 @@ object StreamEx1 extends App {
     println("==>test2")
 
     def buildFibs(n: Int): Vector[BigInt] = {
-      lazy val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map(n => n._1 + n._2)
+      lazy val fibs: LazyList[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map(n => n._1 + n._2)
       fibs.take(n).toVector
     }
 
@@ -30,7 +30,7 @@ object StreamEx1 extends App {
 
   def test3(): Unit = {
     println("==>test3")
-    lazy val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
+    lazy val fibs: LazyList[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
     println(fibs.tail)
     println(fibs.tail.tail)
     println(fibs.take(5))
